0154aefb89e495dc1b61fdba4bd14948cfcd3d93
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-06-20 13:01:39 +0530

    Update config.py

--- Yukki/config.py
+++ Yukki/config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv() # environment
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION_NAME = getenv("SESSION_NAME", "session")
+API_ID = int(getenv("API_ID", "3974784"))
+API_HASH = getenv("API_HASH", "c58c42689bdd066c7e64af20484a34f7")
+BOT_TOKEN = getenv("BOT_TOKEN", "5373933240:AAFyzbScHY0rr4oJwOlcYnThhQ6tcBqW8kc")
+SESSION_NAME = getenv("SESSION_NAME", "BQA4aVPn6amzgXnquTFc7JcSztr9nPRm9dBdsbMR3nfgt1QIMTHcJNji-DNocVHEcV9mKUaKBPwgTqeXjr9mTEStIc_YuwYlitxnALMbVwQD1VTfrXQJAp0Z9HjeNYnGZUyn8167luf_vk-_ULuuYBmvEzeI3MW0r-HaNh-LxGAh4eTv8rih2JWQoenvpqAvXEueGV1oxeeKsPmbRy4ewhrfSUg-kbJnzhPUTZXIwLKLlSwzcDHN_lUuKGHqBZj_8yw-rY9dQ9e4nhyqxnk-IoLgU5uSIRJeEmeRvFaO_we4PdWfkOI-QN61-2uVOfikTHewIEngW0RpOF9qIBtVzEoXAAAAASogKNEA")
 
-ASS_ID = int(getenv("ASS_ID"))
+ASS_ID = int(getenv("5001717969"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "54000"))
